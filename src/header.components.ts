eab1c16b1790b9494947dd1f63c88d83bb0d31b4
Gilles <quodbach.gilles@gmail.com>
2024-10-08 12:11:45 +0200

    first commit

---
+++ src/header.components.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.components.html',
+})
+export class HeaderComponent {}
