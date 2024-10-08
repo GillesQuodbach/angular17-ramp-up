36892d0dab558f1477629243748d3256130519c5
Gilles <quodbach.gilles@gmail.com>
2024-10-08 12:51:16 +0200

    2-28-introducing signals

--- src/app/header/header.components.ts
+++ src/app/header/header.components.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.components.html',
+  styleUrl: './header.components.css',
 })
 export class HeaderComponent {}
